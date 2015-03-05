4ca538d46c6659f7919ea7e9c7e2633bdac4e332
home <bogdan.dudu@activedesign.ro>
2015-03-05 13:36:56 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -56,6 +56,31 @@
 		$('.page').not('.page:last').remove();
 		
 	};
+	function replaceNavigator(origNavigator) {
+	    var CordovaNavigator = function() {};
+	    CordovaNavigator.prototype = origNavigator;
+	    var newNavigator = new CordovaNavigator();
+	    // This work-around really only applies to new APIs that are newer than Function.bind.
+	    // Without it, APIs such as getGamepads() break.
+	    if (CordovaNavigator.bind) {
+		for (var key in origNavigator) {
+		    if (typeof origNavigator[key] == 'function') {
+			newNavigator[key] = origNavigator[key].bind(origNavigator);
+		    } else {
+			(function(k) {
+			    Object.defineProperty(newNavigator, k, {
+				get: function() {
+				    return origNavigator[k];
+				},
+				configurable: true,
+				enumerable: true
+			    });
+			})(key);
+		    }
+		}
+	    }
+	    return newNavigator;
+	}
 	
         var admobid = {};
         if( /(android)/i.test(navigator.userAgent) ) { 
