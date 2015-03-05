681fc9a919b7dbba76106b6d1c067292a4dc58ed
home <bogdan.dudu@activedesign.ro>
2015-03-05 15:00:37 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -55,4 +55,41 @@
 		$('.page').not('.page:last').remove();
 		
 	};
+    var admobid = {};
+        if( /(android)/i.test(navigator.userAgent) ) { 
+            admobid = { // for Android
+                banner: 'ca-app-pub-6869992474017983/9375997553',
+                interstitial: 'ca-app-pub-6869992474017983/1657046752'
+            };
+        } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+            admobid = { // for iOS
+                banner: 'ca-app-pub-6869992474017983/4806197152',
+                interstitial: 'ca-app-pub-6869992474017983/7563979554'
+            };
+        } else {
+            admobid = { // for Windows Phone
+                banner: 'ca-app-pub-6869992474017983/8878394753',
+                interstitial: 'ca-app-pub-6869992474017983/1355127956'
+            };
+        }
+        
+        function initApp() {
+            if (typeof admob !== 'undefined') {
+                //console.log('jaja');
+                setTimeout(function(){
+                    admob.createBannerView({
+                        publisherId : admobid.banner,
+                        //position : AdMob.AD_POSITION.BOTTOM_CENTER,
+                        autoShow : true
+                    });
+                },200);
+                admob.requestInterstitialAd({
+                    publisherId : admobid.interstitial,
+                    //position : AdMob.AD_POSITION.BOTTOM_CENTER,
+                    autoShow : true
+                    });
+            }
+        }
+        
+        document.addEventListener('deviceready', initApp, false);	
 }());
