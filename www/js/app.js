49286c0e6b6a22118600ad0257cb75bb40b1f5f7
home <bogdan.dudu@activedesign.ro>
2015-03-05 15:05:45 +0200

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -32,22 +32,22 @@
     //});
 
     /* --------------------------------- Event Registration -------------------------------- */
-    document.addEventListener('deviceready', function () {
-        StatusBar.overlaysWebView( false );
-        StatusBar.backgroundColorByHexString('#ffffff');
-        StatusBar.styleDefault();
-        FastClick.attach(document.body);
-        if (navigator.notification) { // Override default HTML alert with native dialog
-            window.alert = function (message) {
-                navigator.notification.alert(
-                    message,    // message
-                    null,       // callback
-                    "Workshop", // title
-                    'OK'        // buttonName
-                );
-            };
-        }
-    }, false);
+    //document.addEventListener('deviceready', function () {
+    //    StatusBar.overlaysWebView( false );
+    //    StatusBar.backgroundColorByHexString('#ffffff');
+    //    StatusBar.styleDefault();
+    //    FastClick.attach(document.body);
+    //    if (navigator.notification) { // Override default HTML alert with native dialog
+    //        window.alert = function (message) {
+    //            navigator.notification.alert(
+    //                message,    // message
+    //                null,       // callback
+    //                "Workshop", // title
+    //                'OK'        // buttonName
+    //            );
+    //        };
+    //    }
+    //}, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function clean_doom() {
